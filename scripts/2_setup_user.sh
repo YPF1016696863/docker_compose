4e0c720f05b2e64530f22584380c12b81e646bc8
Wenqing Gu <wenqing.gu@gmail.com>
2019-08-12 21:31:44 +0200

    Adjust to provide mail server details

--- scripts/2_setup_user.sh
+++ scripts/2_setup_user.sh
@@ -4,9 +4,9 @@ curl -i -X POST \
    -H "Content-Type:application/json" \
    -d \
 '{
-  "email":"test@gmail.com",
+  "email":"demo.user.6@zkhoneycomb.com",
   "password":"fcfcfc",
   "org_name":"fc",
-  "name":"Test Account"
+  "name":"测试账号6"
 }' \
  'http://localhost:5000/api/setup'
